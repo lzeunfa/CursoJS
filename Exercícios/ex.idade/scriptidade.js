fa5228bb5d6537e7b45b12d61e15e3c80e0c274a
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-09-05 16:33:39 -0300

    terminei sabomba

--- Exercícios/ex.idade/scriptidade.js
+++ Exercícios/ex.idade/scriptidade.js
@@ -24,33 +24,73 @@ function verificar(){
         var sexo = ``
         var img = document.createElement(`img`)
         img.setAttribute(`id`, `foto`)
+        img.style.borderRadius = `50%`
+        verificado.style.textAlign = `center`
         if(txtsexo[0].checked){
-            sexo = `Homem`
+            sexo = `homem`
             if(idade>=0 && idade<10){
                 //criança
+                img.setAttribute(`src`, `img/bbhomem.jpg`)
+                img.style.width = `300px`
+                img.style.height = `280px`
+                verificado.innerHTML = `<p>Detectamos que você é uma criança ${sexo} e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }else if(idade<=20){
                 //jovem
+                img.setAttribute(`src`, `img/jvhomem.jpg`)
+                img.style.width = `300px`
+                img.style.height = `260px`
+                verificado.innerHTML = `<p>Detectamos que você é um jovem ${sexo} e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }else if(idade<=50){
                 //adulto
+                img.setAttribute(`src`, `img/adthomem.jpg`)
+                img.style.width = `300px`
+                img.style.height = `280px`
+                verificado.innerHTML = `<p>Detectamos que você é um ${sexo} adulto e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }else{
                 //idoso
+                img.setAttribute(`src`, `img/idoso.jpg`)
+                img.style.width = `300px`
+                img.style.height = `280px`
+                verificado.innerHTML = `<p>Detectamos que você é um ${sexo} idoso e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }
 
         }else if(txtsexo[1].checked){
-            sexo = `Mulher`
+            sexo = `mulher`
             if(idade>=0 && idade<10){
                 //criança
+                img.setAttribute(`src`, `img/bbmulher.jpg`)
+                img.style.width = `300px`
+                img.style.height = `265px`
+                verificado.innerHTML = `<p>Detectamos que você é uma criança ${sexo} e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }else if(idade<=20){
                 //jovem
+                img.setAttribute(`src`, `img/jvmulher.jpg`)
+                img.style.width = `300px`
+                img.style.height = `280px`
+                verificado.innerHTML = `<p>Detectamos que você é uma jovem ${sexo} e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }else if(idade<=50){
-                //adulto
+                //adulta
+                img.setAttribute(`src`, `img/adtmulher.jpg`)
+                img.style.width = `300px`
+                img.style.height = `260px`
+                verificado.innerHTML = `<p>Detectamos que você é uma ${sexo} adulta e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }else{
-                //idoso
+                //idosa
+                img.setAttribute(`src`, `img/idosa.jpg`)
+                img.style.width = `300px`
+                img.style.height = `280px`
+                verificado.innerHTML = `<p>Detectamos que você é uma ${sexo} idosa e tem ${idade} anos!</p>`
+                verificado.appendChild(img)
             }
-            
+
         }
-        verificado.style.textAlign = `center`
-        verificado.innerHTML = `<p>Detectamos que você é ${sexo} e tem${idade} anos!</p>`
     }
 
 }
