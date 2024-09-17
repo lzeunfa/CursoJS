7c1c614f9d8be91713872a715d6d9514d1c129ae
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-09-17 19:43:32 -0300

    nova pasta pra nova aula

---
+++ Aula13/escrever.js
@@ -0,0 +1,6 @@
+var contar=0
+
+while(contar<5){
+    contar+=1
+    console.log(`Olá tudo bem pela ${contar} vez`)
+}
---
+++ Aula13/ex.fatia.js
@@ -0,0 +1,8 @@
+var fatia=10
+
+while (fatia>0){
+    console.log(`${fatia} fatias`)
+    fatia-=1
+}
+
+console.log(`${fatia} fatias`)
