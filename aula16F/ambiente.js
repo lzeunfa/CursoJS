7c2084898856f34193736aa43d2a2d4195ad501c
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-09-26 21:08:40 -0300

    novos exercicios

---
+++ aula16F/ambiente.js
@@ -0,0 +1,11 @@
+function parimp(n){
+    if (n%2==0){
+        return `Par!`
+
+    }else{
+        return `Ímpar!`
+    }
+}
+
+var res = parimp()
+console.log(res)
---
+++ aula16F/fatorial.js
@@ -0,0 +1,9 @@
+function fatorial(n){
+    var fat = 1
+    for (c=n; c>1;c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(4))
