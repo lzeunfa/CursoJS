09f240862c9e1fc5824e2d5fddf404dd081755dc
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-10-01 19:38:37 -0300

    finalização do exercicio de 1 a 100

--- Exercícios/N1a100/1a100.js
+++ Exercícios/N1a100/1a100.js
@@ -6,6 +6,11 @@ function adicionar(){
 
     var resultado = document.getElementById(`resultado`)
 
+    var btfinal = document.getElementById(`finalizar`)
+
+    var resp = document.getElementById(`resp`)
+
+
     if (num>100 || num<1){
         window.alert(`[ERRO] respeite as condições solicitadas e insira novamente os dados`)
     }else if (nums.includes(num)){
@@ -14,4 +19,35 @@ function adicionar(){
         nums.push(num)
         resultado.innerHTML += `Número ${num} adicionado <br>`
     }
+
+    if (btfinal.style.display=`none`){
+        btfinal.style.display=`block`
+    }
+
+    nums.sort()
+}
+
+function finalizar(){
+    resp.innerHTML = ``
+
+    var soma = 0
+
+    var media = 0
+
+    if(nums.lenght==0){
+        resp.innerHTML = ``
+    }else{
+        resp.innerHTML += `Ao todo temos ${nums.length} números registrados<br>`
+        resp.innerHTML += `O maior valor informado foi o ${Math.max(...nums)} <br>`
+        resp.innerHTML += `O menor valor informado foi o ${Math.min(...nums)} <br>`
+
+        for(var c=nums.length -1; c>=0; c--){
+            soma += nums[c]
+        }
+        resp.innerHTML += `A soma dos números é ${soma} <br>`
+
+        media = soma/nums.length
+        resp.innerHTML += `A média dos números é ${media} <br>`
+    }
+    
 }
