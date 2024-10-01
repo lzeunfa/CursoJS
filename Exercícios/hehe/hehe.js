4f412d18307328abd1cb197ddd17a7a6b5248db3
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-10-01 20:31:28 -0300

    projeto zoas

---
+++ Exercícios/hehe/hehe.js
@@ -0,0 +1,10 @@
+function rnao(){
+    var nao = document.getElementById(`nao`)
+
+    var txt = document.getElementById(`txt`)
+
+    nao.style.display=`none`
+
+    txt.innerHTML=`Resposta errada, <br> vai ter cuzinho?`
+    txt.style.textAlign=`center`
+}
