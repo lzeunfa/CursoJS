fb0e16fa9aeaeb6b67324a8eb0546a980400a746
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-04 17:32:20 -0300

    Update objects01.js

--- Aula17/objects01.js
+++ Aula17/objects01.js
@@ -1,2 +1,9 @@
-var amigo = {}
-console.log(typeof amigo)
+var amigo = {nome: 'José',
+     sexo: 'M',
+      peso:70,
+       engordar(p){
+        console.log('Engordou')
+        	this.peso += p
+    }}
+amigo.engordar(2)
+console.log(`${amigo.nome} agora pesa ${amigo.peso} Kg`)
